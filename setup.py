84738a52649e36e306be3702b0530ac8669007bf
PayLink Protocol <mail@paylinkprotocol.com>
2025-03-29 17:52:36 +0100

    Adjusted Version Identifier

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="paylink_protocol",
-    version="1.1.1",
+    version="1.1.2",
     description="A Python library for managing the PayLink Protocol on Ethereum.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
