c0f57c0bfcf968e9576e52d77a6ce0cd6ce919b4
PayLink Protocol <mail@paylinkprotocol.com>
2025-03-29 17:52:15 +0100

    Removed Comment

--- src/resources.py
+++ src/resources.py
@@ -8,7 +8,7 @@ _defaultRpcMainnet = "https://rpc.flashbots.net/fast"
 _defaultRpcTestnet = "https://0xrpc.io/sep"
 defaultRpc = _defaultRpcTestnet if _testnetMode else _defaultRpcMainnet
 
-_routerAddressMainnet = "0xFcFB187EF444D7b01253BB4FAF862C32b36E5aB8" # TODO
+_routerAddressMainnet = "0xFcFB187EF444D7b01253BB4FAF862C32b36E5aB8"
 _routerAddressTestnet = "0xFcFB187EF444D7b01253BB4FAF862C32b36E5aB8"
 routerAddress = _routerAddressTestnet if _testnetMode else _routerAddressMainnet
 
